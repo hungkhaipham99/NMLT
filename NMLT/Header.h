f7328d6083a1f2113f5752c80c0bddbd824d9c6b
hungkhaipham <54020845+hungkhaipham@users.noreply.github.com>
2020-12-01 22:50:43 +0700

    Test code VD

--- NMLT/Header.h
+++ NMLT/Header.h
@@ -1,1 +1,33 @@
-#pragma once
+﻿#include <stdio.h>
+#include <conio.h>
+#include <Windows.h>
+#include <string.h>
+using namespace std;
+
+#define BOARD_SIZE 12 // Kích thức ma trận bàn cờ
+#define LEFT 3 // Tọa độ trái màn hình bàn cờ
+#define TOP 1 // Tọa độ trên màn hình bàn cờ
+// Khai báo kiểu dữ liệu
+struct _POINT { int x, y, c; }; // x: tọa độ dòng, y: tọa độ cột, c: đánh dấu
+_POINT _A[BOARD_SIZE][BOARD_SIZE]; //Ma trận bàn cờ
+bool _TURN; //true là lượt người thứ nhất và false là lượt người thứ hai
+int _COMMAND; // Biến nhận giá trị phím người dùng nhập
+int _X, _Y; //Tọa độ hiện hành trên màn hình bàn cờ
+
+
+void FixConsoleWindow();
+void GotoXY(int x, int y);
+void DrawBoard(int pSize);
+void ResetData();
+int ProcessFinish(int pWhoWin);
+int AskContinue();
+void StartGame();
+void GabageCollect();
+void ExitGame();
+int CheckBoard(int pX, int pY);
+void MoveRight();
+void MoveLeft();
+void MoveDown();
+
+void MoveUp();
+int TestBoard();
---
+++ NMLT/main.cpp
@@ -0,0 +1,65 @@
+﻿#include "Header.h"
+
+int main()
+{
+	FixConsoleWindow();
+	StartGame();
+	bool validEnter = true;
+
+	while (1)
+
+	{
+
+		_COMMAND = toupper(_getch());
+
+		if (_COMMAND == 27)
+
+		{
+			ExitGame();
+
+			return 0;
+
+		}
+		else {
+			if (_COMMAND == 'A') MoveLeft();
+			else if (_COMMAND == 'W') MoveUp();
+			else if (_COMMAND == 'S') MoveDown();
+			else if (_COMMAND == 'D') MoveRight();
+			else if (_COMMAND == 13) {// Người dùng đánh dấu trên màn hình bàn cờ
+				switch (CheckBoard(_X, _Y)) {
+
+				case -1:
+
+					printf("X"); break;
+
+				case 1:
+					printf("O"); break;
+				case 0: validEnter = false; // Khi đánh vào ô đã đánh rồi
+
+				}
+				// Tiếp theo là kiểm tra và xử lý thắng/thua/hòa/tiếp tục
+				if (validEnter == true) {
+
+					switch (ProcessFinish(TestBoard())) {
+
+					case -1: case 1: case 0:
+						if (AskContinue() != 'Y') {
+
+							ExitGame();
+							return 0;
+
+						}
+
+						else StartGame();
+
+					}
+
+				}
+				validEnter = true; // Mở khóa
+
+			}
+
+		}
+
+	}
+}
